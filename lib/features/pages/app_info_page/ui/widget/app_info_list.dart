b84ce39a5d6590c46b1a0344b3fe97db2261875e
Mostafa Sensei106 <wwwmostafagmer100@gmail.com>
2025-05-30 22:52:31 +0300

    Changes to be committed: 	modified:   lib/features/pages/app_info_page/ui/widget/app_info_list.dart

--- lib/features/pages/app_info_page/ui/widget/app_info_list.dart
+++ lib/features/pages/app_info_page/ui/widget/app_info_list.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:tadamon/core/config/const/sensei_const.dart';
 import 'package:tadamon/core/widgets/button_component/button_compnent.dart';
 import 'package:tadamon/core/widgets/drawer_component/drawer_component.dart';
 import 'package:tadamon/features/pages/app_info_page/ui/widget/app_info_title.dart';
