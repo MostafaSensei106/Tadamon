9f544a180810619b09de3722bc2d37debdfa5c6b
MostafaSensei106 <wwwmostafagmer100@gmai.cm>
2025-07-29 00:10:59 +0300

    refactor: Remove unused file and update MaterialApp configurations

--- lib/tadamon.dart
+++ lib/tadamon.dart
@@ -31,15 +31,13 @@ class TadamonApp extends StatelessWidget {
       builder: (final context, final themeState) => ToastificationWrapper(
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: ' تَضَامُنٌ',
+          title: 'تَضَامُنٌ',
           theme: lightTheme,
           darkTheme: darkTheme,
           navigatorKey: AppRouter.navigatorKey,
           themeMode: themeState.themeMode,
           initialRoute: Routes.onBoarding,
           onGenerateRoute: appRouter.generateRoute,
-          themeAnimationCurve: Curves.easeInOut,
-
           locale: const Locale('ar', 'EG'),
           supportedLocales: const [Locale('ar', 'EG')],
           localizationsDelegates: const [
