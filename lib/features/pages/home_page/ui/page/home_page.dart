b657d2665794d73344dd806807fe822b7574babf
MostafaSensei106 <wwmostafagmer100@gmail.com>
2025-11-17 13:48:23 +0200

    refactor(home-page): Integrate HomeCubit for state management

--- lib/features/pages/home_page/ui/page/home_page.dart
+++ lib/features/pages/home_page/ui/page/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../core/config/const/sensei_const.dart';
+import '../../logic/cubit/home_cubit.dart';
 import '../widget/home_app_tools.dart';
 import '../widget/home_tip.dart';
 import '../widget/image_news.dart';
@@ -10,10 +12,13 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(final BuildContext context) => const Padding(
-    padding: EdgeInsets.all(SenseiConst.padding),
-    child: Column(
-      children: [HomeTip(), ImageNews(), HomeAppTools(), ItemsCounter()],
+  Widget build(final BuildContext context) => BlocProvider(
+    create: (_) => HomeCubit()..loadContent(),
+    child: const Padding(
+      padding: EdgeInsets.all(SenseiConst.padding),
+      child: Column(
+        children: [HomeTip(), ImageNews(), HomeAppTools(), ItemsCounter()],
+      ),
     ),
   );
 }
